ef3cfe23646f8f91cf9688a421c0b55694e8e2b2
pablo44 <pawelrz@wp.pl>
2023-12-11 20:21:23 +0100

    comments

--- os_own.py
+++ os_own.py
@@ -1,13 +1,14 @@
 import os
 
-
+#creating func to search text in directory
 def search_files(directory, search_text):
-    
+
+#creating en iterator obj of scandir method   
         obj = os.scandir(directory)
-    
+#looping through objects that has inbuld atribute  'name'
         for dt in obj:
             file_path = os.path.join(directory, dt.name)
-
+#checking if the result in file path is a file, reading it, and finaly frinting suscesuly found file
             try:
                 if dt.is_file():
 
@@ -16,10 +17,10 @@ def search_files(directory, search_text):
 
                         if search_text in data_path:
                             print('yes we hav this')
-
+#if object dt is directory we have to again search for files using recursive call of search_files with atributes 'file_path' and 'searchtext'
                 elif dt.is_dir():                
                     search_files(file_path, search_text)
-           
+        #showing the error when the file is not readible
             except Exception as e:
                 print(f"error reading file {file_path}: {e}")
 
